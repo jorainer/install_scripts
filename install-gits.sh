f9e4462447b6d72c2cd43759c98ef1bbb4558ca3
jotsetung <johannes.rainer@gmail.com>
2017-08-03 07:44:56 +0200

    Update git repo install script

--- install-gits.sh
+++ install-gits.sh
@@ -14,23 +14,41 @@ git clone https://github.com/Ensembl/ensembl-io.git
 echo "PERL5LIB=~/ensembl/API/bioperl-live/:~/ensembl/API/ensembl/modules/:~/ensembl/API/ensembl-compara/modules/:~/ensembl/API/ensembl-funcgen/modules/:~/ensembl/API/ensembl-io/modules/:~/ensembl/API/ensembl-variation/modules/:$PERL5LIB" >> ~/.profile
 echo "export PERL5LIB" >> ~/.profile
 
-echo "jotsetung stuff"
+echo "Emacs for OS X"
 mkdir -p ~/Projects/git/
 cd ~/Projects/git
+git clone https://github.com/jimeh/build-emacs-for-osx
+
+echo "jotsetung stuff"
+mkdir -p ~/Projects/git/jotsetung/
+cd ~/Projects/git/jotsetung/
 git clone https://github.com/jotsetung/ensembldb
-git clone https://github.com/jotsetung/FamAgg
 git clone https://github.com/jotsetung/memacs
 git clone https://github.com/jotsetung/atc
-git clone https://github.com/jotsetung/install_scripts
+git clone https://github.com/jotsetung/mzR
 
+mkdir -p ~/Projects/git/sneumann/
+cd ~/Projects/git/sneumann/
 echo "sneumann stuff"
 git clone https://github.com/sneumann/xcms
 git clone https://github.com/sneumann/mzR
 
 echo "lgatto etc stuff"
+mkdir -p ~/Projects/git/lgatto/
+cd ~/Projects/git/lgatto/
 git clone https://github.com/lgatto/MSnbase
+mkdir -p ~/Projects/git/ComputationalProteomicsUnit/
+cd ~/Projects/git/ComputationalProteomicsUnit/
 git clone https://github.com/ComputationalProteomicsUnit/Pbase
 
 echo "Bioconductor stuff"
+mkdir -p ~/Projects/git/Bioconductor/
+cd ~/Projects/git/Bioconductor/
 git clone https://github.com/Bioconductor/BiocStickers
 git cline https://github.com/Bioconductor/AnnotationFilter
+
+echo "EuracBiomedicalResearch stuff"
+mkdir -p ~/Projects/git/EuracBiomedicalResearch/
+cd ~/Projects/git/EuracBiomedicalResearch/
+git clone https://github.com/EuracBiomedicalResearch/FamAgg
+git clone https://github.com/EuracBiomedicalResearch/chrisUtils
