4ed7837a67f23554b75ac4d8600659053127d1fb
jotsetung <johannes.rainer@gmail.com>
2017-09-08 08:53:55 +0200

    Avoid creating tags for Rd and c files.

--- dotags.sh
+++ dotags.sh
@@ -2,9 +2,9 @@
 # options: --no-Rd, --no-c, --no-R
 
 BASEPATH="/Users/jo/Projects/git"
-DIRS=( "lgatto/MSnbase" "jotsetung/atc" "jotsetung/chrisUtils" "jotsetung/ensembldb" "ggbio" "jotsetung/mirtarbase" "jotsetung/mirhostgenes" "sneumann/xcms" "Bioconductor/AnnotationFilter" "sneumann/mzR" "jotsetung/FamAgg")
+DIRS=( "lgatto/MSnbase" "jotsetung/atc" "jotsetung/chrisUtils" "jotsetung/ensembldb" "jotsetung/mirtarbase" "jotsetung/mirhostgenes" "sneumann/xcms" "Bioconductor/AnnotationFilter" "jotsetung/FamAgg" "jotsetung/mzR")
 for i in "${DIRS[@]}"
 do
-    R CMD rtags -V -o "$BASEPATH/TAGS" --append "$BASEPATH/$i"
+    R CMD rtags -V --no-Rd --no-c -o "$BASEPATH/TAGS" --append "$BASEPATH/$i"
 done
 
