b6157d5146883856c38867fa0dcf3d4ec92662b7
jotsetung <johannes.rainer@gmail.com>
2017-11-06 05:50:46 +0100

    Modify the brewer

--- brew-jo-strong.sh
+++ brew-jo-strong.sh
@@ -109,6 +109,8 @@ echo "\n\nDone\n\n"
 echo "________________________________________________________________________________"
 echo "Installing python:\n"
 brew install python
+brew unlink python && brew link python
+## wget https://bootstrap.pypa.io/get-pip.py
 pip install --upgrade setuptools
 pip install --upgrade pip
 pip install numpy
