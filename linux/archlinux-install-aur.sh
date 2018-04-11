eed93cac2d0cbd1c679933be35a27833d6da3ab9
jotsetung <johannes.rainer@gmail.com>
2018-04-11 10:05:42 +0000

    Add install scripts for archlabs/archlinux

---
+++ linux/archlinux-install-aur.sh
@@ -0,0 +1,23 @@
+echo "Installing AUR stuff"
+mkdir -p ~/builds
+
+echo "mysql56"
+cd ~/builds
+git clone https://aur.archlinux.org/mysql56.git
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/mysql56.tar.gz
+cd mysql56
+makepkg -sic
+
+echo "plex-media-player"
+cd ~/builds
+git clone https://aur.archlinux.org/plex-media-player.git
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/plex-media-player.tar.gz
+cd plex-media-player
+makepkg -sic
+
+echo "filebot"
+cd ~/builds
+git clone https://aur.archlinux.org/filebot47.git
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/filebot47.tar.gz
+cd filebot47
+makepkg -sic
---
+++ linux/archlinux-install.sh
@@ -0,0 +1,55 @@
+## This assumes we have an installation of archlinux running.
+## Additional settings and stuff useful for customization:
+## https://github.com/Dobbie03/dots
+## Missing:
+## - udunits2
+echo "This script should be run with sudo"
+
+pacman -Sy
+
+echo "Installing system tools"
+pacman -S --noconfirm pkgfile
+## pkgfile -u and then pkgfile netstat or similar
+pacman -S --noconfirm mc
+pacman -S --noconfirm htop
+pacman -S --noconfirm cifs-utils dosfstools mtools ntfs-3g
+pacman -S --noconfirm wicd
+pacman -S --noconfirm gkrellm
+pacman -S --noconfirm owncloud-client
+pacman -S --noconfirm xdotool xsel keepass
+pacman -S --noconfirm terminator
+
+echo "Installing development tools"
+pacman -S --noconfirm gcc gcc-gfortran gcc-objc
+pacman -S --noconfirm graphviz
+pacman -S --noconfirm perl python
+pacman -S --noconfirm libxml2
+pacman -S --noconfirm sqlite
+pacman -S --noconfirm readline
+pacman -S --noconfirm git gist
+pacman -S --noconfirm netcdf netcdf-cxx
+pacman -S --noconfirm imagemagick6
+pacman -S --noconfirm perlbrew
+pacman -S --noconfirm pkg-config cmake automake autoconf
+pacman -S --noconfirm emacs
+pacman -S --noconfirm openvpn openssl
+pacman -S --noconfirm fftw units
+pacman -S --noconfirm jdk10-openjdk java-openjfx
+pacman -S --noconfirm gsl
+pacman -S --noconfirm python-pip python-setuptools python-setuptools-git
+
+echo "Installing productivity tools"
+pacman -S --noconfirm texlive-bin texlive-core texlive-fontsextra texlive-science
+pacman -S --noconfirm biber
+pacman -S --noconfirm texinfo texi2html 
+pacman -S --noconfirm pandoc pandoc-citeproc pandoc-crossref
+pacman -S --noconfirm libreoffice-still
+pacman -S --noconfirm ttf-inconsolata
+
+echo "Installing multimedia packages"
+pacman -S --noconfirm gimp darktable
+pacman -S --noconfirm vlc mpv
+pacman -S --noconfirm ffmpeg ffmpegthumbnailer
+pacman -S --noconfirm ario xmms2
+pacman -S --noconfirm handbrake
+
