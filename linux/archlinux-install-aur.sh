849731ce1b6e780748562947cbfd997fad438e97
jotsetung <johannes.rainer@gmail.com>
2018-04-13 09:55:10 +0200

    Configure zsh in archlinux-install-aur.sh

--- linux/archlinux-install-aur.sh
+++ linux/archlinux-install-aur.sh
@@ -28,3 +28,17 @@ git clone https://aur.archlinux.org/udunits.git
 curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/udunits.tar.gz
 cd udunits
 makepkg -sic
+
+echo "Configuring zsh etc."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+cat "$HOME/.zshrc" >> "$HOME/.zprezto/runcoms/zshrc"
+rm "$HOME/.zshrc"
+ln -s "$HOME/.zprezto/runcoms/zlogin" "$HOME/.zlogin" 
+ln -s "$HOME/.zprezto/runcoms/zlogout" "$HOME/.zlogout"
+ln -s "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
+ln -s "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
+ln -s "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
+ln -s "$HOME/.zprezto/runcoms/zshrc" "$HOME/.zshrc"
+echo "alias R='R --no-save'" >> "$HOME/.zshrc"
+git clone https://github.com/seebi/dircolors-solarized "${ZDOTDIR:-$HOME}/.zprezto/contrib/dircolors-solarized/"
+echo "eval \`dircolors ~/.zprezto/contrib/dircolors-solarized/dircolors.256dark\`" >> "$HOME/.zshrc"
--- linux/archlinux-install.sh
+++ linux/archlinux-install.sh
@@ -54,18 +54,3 @@ pacman -S --noconfirm ffmpeg ffmpegthumbnailer
 pacman -S --noconfirm ario xmms2
 pacman -S --noconfirm handbrake
 
-echo "Configuring zsh etc."
-su jo
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-cat "$HOME/.zshrc" >> "$HOME/.zprezto/runcoms/zshrc"
-rm "$HOME/.zshrc"
-ln -s "$HOME/.zprezto/runcoms/zlogin" "$HOME/.zlogin" 
-ln -s "$HOME/.zprezto/runcoms/zlogout" "$HOME/.zlogout"
-ln -s "$HOME/.zprezto/runcoms/zpreztorc" "$HOME/.zpreztorc"
-ln -s "$HOME/.zprezto/runcoms/zprofile" "$HOME/.zprofile"
-ln -s "$HOME/.zprezto/runcoms/zshenv" "$HOME/.zshenv"
-ln -s "$HOME/.zprezto/runcoms/zshrc" "$HOME/.zshrc"
-echo "alias R='R --no-save'" >> "$HOME/.zshrc"
-git clone https://github.com/seebi/dircolors-solarized "${ZDOTDIR:-$HOME}/.zprezto/contrib/dircolors-solarized/"
-echo "eval \`dircolors ~/.zprezto/contrib/dircolors-solarized/dircolors.256dark\`" >> "$HOME/.zshrc"
-exit
