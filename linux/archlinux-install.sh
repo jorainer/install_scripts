37231c8b81dc015ac94b4fcb86cbabef8f7cb795
jotsetung <johannes.rainer@gmail.com>
2018-05-01 07:14:04 +0200

    Add digikam package

--- linux/archlinux-install.sh
+++ linux/archlinux-install.sh
@@ -58,6 +58,7 @@ pacman -S --noconfirm ffmpeg ffmpegthumbnailer
 pacman -S --noconfirm ario xmms2
 pacman -S --noconfirm handbrake
 pacman -S --noconfirm transmission-gtk
+pacman -S --noconfirm digikam
 
 echo "Switching to JDK8"
 archlinux-java java-8-openjdk
