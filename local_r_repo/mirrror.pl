e9c47ce23d13cce82ca9e22727aec8c34da59efd
jorainer <johannes.rainer@gmail.com>
2020-09-23 19:19:14 +0200

    Small fix

--- local_r_repo/mirrror.pl
+++ local_r_repo/mirrror.pl
@@ -23,7 +23,7 @@ getopts("l:r:s:h",\%option);
 my $switch = "bioc";
 ## define which files/folders we would like to exclude.
 ##my $excludes = "--exclude '**windows*' --exclude '**macosx/' --exclude '*data/experiment*'";
-my $excludes = "--exclude '**windows*' --exclude '**macosx/' --exclude '**linux*' --exclude 'CRAN/bin*' --exclude 'bioc/bin*' --exclude 'bioc/citations*' --exclude 'bioc/html*' --exclude 'bioc/install*' --exclude 'bioc/licenses*' --exclude 'bioc/manuals*' --exclude 'bioc/news*' --exclude 'bioc/readmes*' --exclude 'bioc/vignettes*' --exclude 'contrib/Archive*'";
+my $excludes = "--exclude '**windows*' --exclude '**macosx/' --exclude '**linux*' --exclude 'bin*' --exclude 'bioc/bin*' --exclude 'bioc/citations*' --exclude 'bioc/html*' --exclude 'bioc/install*' --exclude 'bioc/licenses*' --exclude 'bioc/manuals*' --exclude 'bioc/news*' --exclude 'bioc/readmes*' --exclude 'bioc/vignettes*' --exclude 'contrib/Archive*'";
 
 
 if( $option{ h } ){
