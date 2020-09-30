588450d77211d8ecb30d639b5259b2a52652cc6e
jorainer <johannes.rainer@gmail.com>
2020-09-30 16:32:46 +0200

    Support skipping package updates

--- check-update-packages.R
+++ check-update-packages.R
@@ -1,8 +1,8 @@
 #' Simple function that tries to load each installed package and if loading
 #' fails installs it with `BiocManager::install`.
-check_update_packages <- function() {
+check_update_packages <- function(skip = "pathview") {
     pkgs <- installed.packages()
-    skip <- c("pathview")
+    pkgs <- pkgs[!pkgs %in% skip]
     pkgs <- pkgs[grep("host-site-library", pkgs[, "LibPath"]), "Package"]
     for (i in seq_along(pkgs)) {
         message("=> ", pkgs[i])
